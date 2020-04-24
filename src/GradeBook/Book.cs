41462a628a86d9bf0a1345b11e37b571b796650d
emma <emma.wishart@live.com.au>
2020-04-24 14:32:16 +0200

    adding a class

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+
+    //creating a new class for abstraction in the Program class
+
+    //Things to think about when creating a new class 
+    //  - what is the behaviour of this particular class?
+    //  - what is the state that is going to be stored inside of instances of this type/class?
+    //  - think of a class as 2 things - the state/data it holds and the behaviour that acts on that state
+   
+    class Book 
+    {
+        //Use the void Keyword with a method that is not going to return an
+        public void AddGrade(List<double> grades){
+
+        }
+
+
+    }
+}
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
+//It is important to put your code in a name space otherwise it is in the global name space and this is dangerous b/c it can lead to conflicts
 namespace GradeBook
 {
     class Program
@@ -15,15 +16,18 @@ namespace GradeBook
             //The System namespace is not required explicitly here as in line one we are 'using' the System Namespace with a 'using statement'
             System.Console.WriteLine(adding);
 
+            var book = new Book();
+            book.AddGrade(89.1);
 
             //List is not in the System namespace, to find what namespace a class is in do the following:
             //place your cursor on the class name and press ctrl+. then click on the "using ....." and it will add this as a 'using statement' at the top
             //A List type requires one type argument, this is put in the <>
             //You can use List similarly to an Array but the benefit of a List over an Array is that you can easily add things into a list (you can add things into an Array also, but it is not as easy). 
             //A list is implemented from the start as a dynamically sizable collection that you can easily add to or take from
+            //A List is storing data(aka state), in this case, the numbers
             List<double> grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
             grades.Add(56.1);
-        
+            
 
             var result = 0.0;
             foreach(var number in grades){
@@ -34,14 +38,6 @@ namespace GradeBook
             //The :N2 is to say how many decimals I would like in my floating number
             Console.WriteLine($"The average grade is: {result:N2}");
 
-
-            if (args.Length >0)
-            {
-                Console.WriteLine($"Hello {args[0]}!");
-            }
-            else {
-                Console.WriteLine("Hello!");
-            }
         }   
     }
 }
