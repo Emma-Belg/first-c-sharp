61e4b77706a1797e0191f5409307f024adfae5c3
emma <emma.wishart@live.com.au>
2020-04-24 13:16:09 +0200

    learning about Lists

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -9,9 +10,20 @@ namespace GradeBook
             double x = 34.1;
             double y = 32.9;
             var result = x + y;
+            //System is the namespace that Console lives in.
+            //When you use the cw shortcut VSC inserts the namespace as well
+            //The System namespace is not required explicitly here as in line one we are 'using' the System Namespace with a 'using statement'
             System.Console.WriteLine(result);
 
             var numbers = new[] {12.7, 10.3, 6.11, 4.1};
+            //List is not in the System namespace, to find what namespace a class is in do the following:
+            //place your cursor on the class name and press ctrl+. then click on the "using ....." and it will add this as a 'using statement' at the top
+            //A List type requires one type argument, this is put in the <>
+            //You can use List similarly to an Array but the benefit of a List over an Array is that you can easily add things into a list (you can add things into an Array also, but it is not as easy). 
+            //A list is implemented from the start as a dynamically sizable collection that you can easily add to or take from
+            List<double> grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
+            grades.Add(56.1);
+        
 
             var result1 = 0.0;
             foreach(var number in numbers){
@@ -27,6 +39,6 @@ namespace GradeBook
             else {
                 Console.WriteLine("Hello!");
             }
-        }
+        }   
     }
 }
