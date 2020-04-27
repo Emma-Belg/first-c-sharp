0e2679d38c5d568669fb125e00f45d02ed437492
emma <emma.wishart@live.com.au>
2020-04-27 16:47:10 +0200

    adding a picture to explain passing by reference

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -8,15 +8,30 @@ namespace GradeBook.Tests
     public class TypeTests
     {
         [Fact]
-        public void Test1()
+        public void CSharpCanPassByRef()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(ref book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        private void GetBookSetName(ref Book book, string name)
+        {
+            book = new Book(name);
+            book.Name = name;
+        }
+
+        [Fact]
+        public void CSharpIsPassByValue()
         {
             var book1 = GetBook("Book 1");
             GetBookSetName(book1, "New Name");
 
-            Assert.Equal("Book 1", "New Name");
+            Assert.Equal("Book 1", book1.Name);
         }
 
-        private void GetBookSetName(Book book1, string name)
+        private void GetBookSetName(Book book, string name)
         {
             book = new Book(name);
             book.Name = name;
